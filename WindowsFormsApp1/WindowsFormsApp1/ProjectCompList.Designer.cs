811e9c1d286c4b6aa430ca15c94bddd0164ae847
Petyamester3343 <tothpepi@gmail.com>
2024-05-15 14:19:47 +0200

    update

--- WindowsFormsApp1/WindowsFormsApp1/ProjectCompList.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/ProjectCompList.Designer.cs
@@ -29,10 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProjektKod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SzuksegesAlkatresz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Oszlop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Polc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rekesz = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -47,13 +53,16 @@
             this.panel1.Size = new System.Drawing.Size(166, 452);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // button1
             // 
-            this.panel2.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.panel2.Location = new System.Drawing.Point(167, 1);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(632, 56);
-            this.panel2.TabIndex = 1;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(30, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Listázás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -68,24 +77,59 @@
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Projekthez tartozó\r\nalkatrészek";
             // 
-            // button1
+            // panel2
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(30, 180);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Listázás";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel2.Location = new System.Drawing.Point(167, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(632, 56);
+            this.panel2.TabIndex = 1;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProjektKod,
+            this.SzuksegesAlkatresz,
+            this.Sor,
+            this.Oszlop,
+            this.Polc,
+            this.Rekesz});
             this.dataGridView1.Location = new System.Drawing.Point(167, 54);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(632, 399);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ProjektKod
+            // 
+            this.ProjektKod.HeaderText = "ProjektKod";
+            this.ProjektKod.Name = "ProjektKod";
+            // 
+            // SzuksegesAlkatresz
+            // 
+            this.SzuksegesAlkatresz.HeaderText = "SzuksegesAlkatresz";
+            this.SzuksegesAlkatresz.Name = "SzuksegesAlkatresz";
+            // 
+            // Sor
+            // 
+            this.Sor.HeaderText = "Sor";
+            this.Sor.Name = "Sor";
+            // 
+            // Oszlop
+            // 
+            this.Oszlop.HeaderText = "Oszlop";
+            this.Oszlop.Name = "Oszlop";
+            // 
+            // Polc
+            // 
+            this.Polc.HeaderText = "Polc";
+            this.Polc.Name = "Polc";
+            // 
+            // Rekesz
+            // 
+            this.Rekesz.HeaderText = "Rekesz";
+            this.Rekesz.Name = "Rekesz";
             // 
             // ProjectCompList
             // 
@@ -111,5 +155,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProjektKod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SzuksegesAlkatresz;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Oszlop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Polc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rekesz;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/ProjectCompList.cs
+++ WindowsFormsApp1/WindowsFormsApp1/ProjectCompList.cs
@@ -7,25 +7,92 @@ namespace WindowsFormsApp1
 {
     public partial class ProjectCompList : Form
     {
+        MySqlConnection con = new MySqlConnection(@"datasource=127.0.0.1;port=3306;username=root;password=;database=napelem");
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable table;
         private readonly string conn = @"datasource=127.0.0.1;port=3306;username=root;password=;database=napelem";
         private readonly string query = "SELECT * FROM projektraktar";
 
+        
         public ProjectCompList()
         {
             InitializeComponent();
+        }
+        private void LoadData()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns[0].Name = "ProjektKod";
+            dataGridView1.Columns[0].HeaderText = "ProjektKod";
+            dataGridView1.Columns[0].DataPropertyName = "ProjektKod";
+            dataGridView1.Columns[1].Name = "ANev";
+            dataGridView1.Columns[1].HeaderText = "ANev";
+            dataGridView1.Columns[1].DataPropertyName = "ANev";
+            dataGridView1.Columns[2].Name = "Sor";
+            dataGridView1.Columns[2].HeaderText = "Sor";
+            dataGridView1.Columns[2].DataPropertyName = "Sor";
+            dataGridView1.Columns[3].Name = "Oszlop";
+            dataGridView1.Columns[3].HeaderText = "Oszlop";
+            dataGridView1.Columns[3].DataPropertyName = "Oszlop";
+            dataGridView1.Columns[4].Name = "Polc";
+            dataGridView1.Columns[4].HeaderText = "Polc";
+            dataGridView1.Columns[4].DataPropertyName = "Polc";
+            dataGridView1.Columns[5].Name = "Rekesz";
+            dataGridView1.Columns[5].HeaderText = "Rekesz";
+            dataGridView1.Columns[5].DataPropertyName = "Rekesz";
+
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    // Az adatbázis kapcsolat nyitva van
+                    // Ide írd a teendőket, amelyeket a kapcsolat nyitva lévő állapotban szeretnél végrehajtani
+                }
+                else
+                {
+                    con.Open();
+                    // Az adatbázis kapcsolat zárva van
+                    // Ide írd a teendőket, amelyeket a kapcsolat zárva lévő állapotban szeretnél végrehajtani
+                }
+                cmd = new MySqlCommand("SELECT * FROM projektraktar INNER JOIN raktar ON projektraktar.ANev = raktar.ANev;", con);
+                adapter = new MySqlDataAdapter(cmd);
+                table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                dataGridView1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt az adatok betöltése közben: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e) => GetProjCompList();
 
         private DataTable GetProjCompList()
         {
-            var con = new MySqlConnection(conn);
-            con.Open();
-            var cmd = new MySqlCommand(query, con);
-            var dr = cmd.ExecuteReader();
             DataTable dt = new DataTable();
-            dt.Load(dr);
+            using(var con = new MySqlConnection(conn))
+            {
+                using(var cmd = new MySqlCommand(query, con))
+                {
+                    con.Open();
+                    var dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                    LoadData();
+                }
+            }
+
             return dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
